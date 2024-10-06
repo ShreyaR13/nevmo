9ba645b606c54d0492ea60aa8964ec3b371314f4
ShreyaR13 <s4.shre@gmail.com>
2024-10-06 15:27:16 -0400

    update Render CORS origin

--- nevmo-backend/app/main.py
+++ nevmo-backend/app/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 # cors setting for FE and BE communication
 origins = [
-    "https://nevmo-react-frontend.onrender.com/",
+    "https://nevmo-react-frontend.onrender.com",
     "http://localhost:3000"
 ]
 
